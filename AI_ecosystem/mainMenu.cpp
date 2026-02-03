dcc688cc6bd194bd5a37d02638be065a521353c0
Emp3ror <ngirard@gaming.tech>
2026-02-03 13:28:39 +0100

    first menu commit so i can couple texture + menu to advance

---
+++ AI_ecosystem/mainMenu.cpp
@@ -0,0 +1,22 @@
+#include "mainMenu.h"
+
+void mainMenu::render(sf::RenderWindow& window, textures& gameTextures)
+{
+	SDL_RenderFillRect(window, nullptr);
+	backgroundTexture = gameTextures.getTexture(textures::texturesIndices::backgroundMenu);
+	SDL_FRect backgroundRect;
+	backgroundRect.x = 960 - ((backgroundTexture->w * 540) / backgroundTexture->h);
+	backgroundRect.y = 0;
+	backgroundRect.w = (backgroundTexture->w * 540) / backgroundTexture->h;
+	backgroundRect.h = 540;
+	SDL_RenderTexture(window, backgroundTexture, nullptr, &backgroundRect);
+
+	for (int i = 0; i < btnTable.size(); ++i)
+	{
+		btnTable[i].draw(window);
+	}
+
+	renderTexture(window, gameTextures, textures::textsIndices::PLAY, 10, 100, 100, 60);
+
+	renderTexture(window, gameTextures, textures::textsIndices::SETTINGS, 10, 200, 220, 60);
+}
---
+++ AI_ecosystem/menu.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <vector>
+#include <string>
+
+#include "btn.h"
+#include "textures.h"
+
+class menu
+{
+protected:
+
+	int addBtn(int x, int y, const std::string& name); //Allows to set a method for the childs to create their own buttons
+
+	void renderTexture(sf::RenderWindow& window, textures gameTextures, textures::textsIndices index, float x, float y, float w, float h); //Used to render the buttons with there parameters
+
+	std::vector<Btn> btnTable; //Create the table where the menus child's can store their btn
+
+	sf::Texture& backgroundTexture;
+
+public:
+	menu() = default; //constructor
+	virtual ~menu() = default; //destructor
+
+	virtual void onBtnPressed(int btnId) = 0; //function used by children to determine the effect off buttons when pressed
+
+	inline const std::vector<Btn>& getButtons() const //function to share the buttons and give them
+	{
+		return btnTable;
+	}
+
+	virtual void render(sf::RenderWindow& window, textures& gameTextures) = 0; //function so that children are able to render their buttons
+
+	void handleMenuEvent(sf::Event& Event);
+};
